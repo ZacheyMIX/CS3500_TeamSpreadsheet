ac003c7e28b449f7dd8302c57fee1c11ccd52fce
Borgerr <Borgerr@DESKTOP-T37S5ON>
2022-10-15 18:17:56 -0600

    comments and some saving/loading functionality

--- Spreadsheet/PS6Skeleton/MainPage.xaml.cs
+++ Spreadsheet/PS6Skeleton/MainPage.xaml.cs
@@ -10,6 +10,11 @@ public partial class MainPage : ContentPage
 {
 
     /// <summary>
+    /// internal Spreadsheet model class. Represents the logic of the spreadsheet.
+    /// </summary>
+    private Spreadsheet model;
+
+    /// <summary>
     /// Constructor for the demo
     /// </summary>
 	public MainPage()
@@ -25,21 +30,34 @@ public partial class MainPage : ContentPage
         spreadsheetGrid.SetSelection(2, 3);
     }
 
+    /// <summary>
+    /// Event for when a cell is selected.
+    /// Clicking a cell has the same meaning as selecting a cell.
+    /// </summary>
     private void displaySelection(SpreadsheetGrid grid)
     {
+        
         spreadsheetGrid.GetSelection(out int col, out int row);
         spreadsheetGrid.GetValue(col, row, out string value);
         if (value == "")
         {
+            /*
             spreadsheetGrid.SetValue(col, row, DateTime.Now.ToLocalTime().ToString("T"));
             spreadsheetGrid.GetValue(col, row, out value);
             DisplayAlert("Selection:", "column " + col + " row " + row + " value " + value, "OK");
+            */
+            spreadsheetGrid.SetValue(col, row, "clicked");
+            spreadsheetGrid.GetValue(col, row, out value);
         }
     }
 
+    /// <summary>
+    /// Effectively creates a new spreadsheet by clearing all entries in the current SpreadsheetGrid.
+    /// </summary>
     private void NewClicked(Object sender, EventArgs e)
     {
         spreadsheetGrid.Clear();
+        model = new();  // MAKE SURE TO CHANGE ON RELEASES
     }
 
     /// <summary>
@@ -58,6 +76,7 @@ public partial class MainPage : ContentPage
 
                 string fileContents = File.ReadAllText(fileResult.FullPath);
                 Console.WriteLine("First 100 file chars:\n" + fileContents.Substring(0, 100));
+                model = new(fileResult.ToString(), s => true, s => s, "default");   // MAKE SURE TO CHANGE ON RELEASES
             }
             else
             {
@@ -71,13 +90,23 @@ public partial class MainPage : ContentPage
         }
     }
 
+    /// <summary>
+    /// Standard "Save As" functionality for spreadsheet programs.
+    /// Should effectively save the current spreadsheet to a new file.
+    /// </summary>
     private async void SaveAsClicked(Object sender, EventArgs e)
     {
-        
+        FileResult fileResult = await FilePicker.Default.PickAsync();   // placeholder so the compiler doesn't throw a fit
+        //model.Save(fileResult.ToString());    // something like that goes here
     }
 
+    /// <summary>
+    /// Standard "Save" functionality for spreadsheet programs.
+    /// Should effectively save the current spreadsheet to a new file.
+    /// </summary>
     private async void SaveClicked(Object sender, EventArgs e)
     {
-        
+        FileResult fileResult = await FilePicker.Default.PickAsync();   // placeholder so the compiler doesn't throw a fit
+        //model.Save(fileResult.ToString());    // something like that goes here
     }
 }
